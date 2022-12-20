67fc3d8fb51b735166ba922362a9a06ecee48ce0
kkanto <108447856+kkanto@users.noreply.github.com>
2022-12-20 09:33:26 +0100

    Create maven project

---
+++ src/main/java/de/allianz/tESTPROJBLA1223/TESTPROJBLA1223Application.java
@@ -0,0 +1,13 @@
+package de.allianz.tESTPROJBLA1223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TESTPROJBLA1223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TESTPROJBLA1223Application.class, args);
+	}
+
+}
